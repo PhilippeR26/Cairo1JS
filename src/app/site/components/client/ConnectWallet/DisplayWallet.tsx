96f80fe9fb11ab486bf3ecc7c1be925a44d4e71f
PhilippeR26 <philippe.rostan@yahoo.fr>
2024-04-23 16:33:05 +0200

    feat: add DisplayWallet

---
+++ src/app/site/components/client/ConnectWallet/DisplayWallet.tsx
@@ -0,0 +1,27 @@
+import { Box } from "@chakra-ui/react"
+import styles from '../../../page.module.css'
+
+export interface StateWallet {
+    addressAccount:string,
+    chainId:string,
+    isConnected:boolean
+}
+
+interface WalletProps {
+    walletData: StateWallet
+}
+// type Props = { walletData: string };
+export default  function WalletDisplay( {walletData}:WalletProps) {
+   return (
+       <>
+       <Box bg='pink.200' color='black' borderWidth='1px' borderRadius='md'>
+           <p className={styles.text1}>
+           
+               address = {walletData.addressAccount} <br />
+               chain = {walletData.chainId}<br />
+               isConnected={walletData.isConnected ? "Yes" : "No"}<br />
+           </p>
+       </Box>
+       </>
+   )
+}
