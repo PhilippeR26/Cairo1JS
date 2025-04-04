7cf819ca4b947c36cfe6f0e645706ce09d59a250
PhilippeR26 <philippe.rostan@yahoo.fr>
2025-04-04 12:53:16 +0200

    feat: test of deploy contract

--- src/app/components/client/Contract/PlayWithCairo1.tsx
+++ src/app/components/client/Contract/PlayWithCairo1.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import {  Contract,  InvokeFunctionResponse, type GetTransactionReceiptResponse } from "starknet";
+import { CallData, Contract, InvokeFunctionResponse, RpcProvider, type Abi, type DeployContractUDCResponse, type GetTransactionReceiptResponse } from "starknet";
 
 import { useStoreBlock } from "../Block/blockContext";
 import { useStoreWallet } from '../ConnectWallet/walletContext';
@@ -10,35 +10,60 @@ import styles from '../../../page.module.css'
 import { test1Abi } from "../../../contracts/abis/test1";
 import TransactionStatus from '../Transaction/TransactionStatus';
 import { addrTESTCONTRACT } from '@/type/constants';
+import { useFrontendProvider } from '../provider/providerContext';
+import { myFrontendProviders } from '@/utils/constants';
 
 const contractAddress = addrTESTCONTRACT;
 
 export default function PlayWithCairo1() {
+    const contractClass = "0x025a178bc9ace058ab1518392780610665857dfde111e1bed4d69742451bc61c" as const;
     // wallet context
     const walletAccountFromContext = useStoreWallet(state => state.myWalletAccount);
 
     // block context
     const blockFromContext = useStoreBlock(state => state.dataBlock);
 
+    // provider context
+    const currentFrontendProviderIndex = useFrontendProvider(state => state.currentFrontendProviderIndex);
+    const readProvider = myFrontendProviders[currentFrontendProviderIndex];
+
     // Component context
     const [balance, setBalance] = useState<number>(0);
     const [transactionHash, setTransactionHash] = useState<string>("");
-    const [_transactionResult, setTransactionResult] = useState<GetTransactionReceiptResponse|undefined>(undefined);
+    const [_transactionResult, setTransactionResult] = useState<GetTransactionReceiptResponse | undefined>(undefined);
 
-    const [cairo1Contract, _setcairo1Contract] = useState<Contract>(new Contract(test1Abi, contractAddress, walletAccountFromContext));
+    const [cairo1Contract, _setCairo1Contract] = useState<Contract>(new Contract(test1Abi, contractAddress, walletAccountFromContext));
+    const [contractAbi, setContractAbi] = useState<Abi>([""]);
 
-    async function increaseBalance() {
-        console.log("increase-Cairo1ReadContract=", cairo1Contract.functions);
-        const myCall = cairo1Contract.populate("increase_counter", [10]);
-        console.log("Call=", myCall);
-        walletAccountFromContext?.execute(myCall)
-            .then(async (resp: InvokeFunctionResponse) => {
-                console.log("increaseBalance txH =", resp.transaction_hash);
-                setTransactionHash(resp.transaction_hash);
-                setTransactionResult(await walletAccountFromContext.waitForTransaction(resp.transaction_hash));
-            })
-            .catch((e: any) => { console.log("error increase balance =", e) });
-    }
+    async function deployContract() {
+        console.log("deploy in progress...");
+        if (walletAccountFromContext){
+            const contractCallData=new CallData(contractAbi);
+            const constructorCalldata=contractCallData.compile(
+                "constructor",{
+                    creator:"0x123456",
+                    factory: "0x4567",
+                    title: "Zorg is back",
+                    description: "e-beggar",
+                    ip_metadata:"addr/addr2",
+                    signers:["0x456","567"],
+                }
+            );
+            walletAccountFromContext.deployContract({
+                classHash: contractClass,
+                constructorCalldata: constructorCalldata
+                }).then(
+                    async (resp: DeployContractUDCResponse) => {
+                        console.log("deploy performed with txH =", resp.classHash);
+                        console.log("deployed at =", resp.address);
+                        setTransactionHash(resp.transaction_hash);
+                        setTransactionResult(await walletAccountFromContext.waitForTransaction(resp.transaction_hash));
+                    }
+                )
+                .catch((e: any) => { console.log("error deploy =", e) });
+
+        }
+     }
 
     useEffect(() => {
         cairo1Contract.get_balance()
@@ -51,15 +76,28 @@ export default function PlayWithCairo1() {
     }
         , [blockFromContext.block_number]); // balance updated at each block
 
+    useEffect(() => {
+        readProvider.getClassByHash(contractClass)
+            .then((resp) => {
+                console.log("resp abi =", resp.abi)
+                setContractAbi(resp.abi as Abi);
+            })
+            .catch((e: any) => { console.log("error get abi =", e) });
+        return () => { }
+
+    }
+        , []
+    )
+
 
     return (
-        <Box 
-        bg='mediumaquamarine' 
-        color='black' 
-        borderWidth='1px' 
-        borderRadius='md' 
-        paddingBottom='3px'
-        marginBottom={20}
+        <Box
+            bg='mediumaquamarine'
+            color='black'
+            borderWidth='1px'
+            borderRadius='md'
+            paddingBottom='3px'
+            marginBottom={20}
         >
             {
                 !balance ? (
@@ -78,16 +116,16 @@ export default function PlayWithCairo1() {
                                     boxShadow="none !important"
                                     bg='green.100'
                                     onClick={() => {
-                                        increaseBalance();
+                                        deployContract();
                                     }}
                                 >
-                                    Increase balance (+10)
+                                    deploy contract
                                 </Button>
                             </Center>
                         </div>
                         {!!transactionHash && (
                             <Box bg='green.300' color='black' borderWidth='1px' borderColor='green.800' borderRadius='md' p={1} margin={2}>
-                                
+
                                 <TransactionStatus transactionHash={transactionHash}></TransactionStatus>
                             </Box>
                         )
