6e614eb50d85a6b643364a6b83c67e093df7948c
PhilippeR26 <philippe.rostan@yahoo.fr>
2025-07-21 14:25:37 +0200

    fix: problem of contract initialization (wrong rpc url)

--- src/app/(site)/components/client/Contract/PlayWithCairo1.tsx
+++ src/app/(site)/components/client/Contract/PlayWithCairo1.tsx
@@ -25,28 +25,47 @@ export default function PlayWithCairo1() {
     const [transactionHash, setTransactionHash] = useState<string>("");
     const [_transactionResult, setTransactionResult] = useState<GetTransactionReceiptResponse | undefined>(undefined);
 
-    const [cairo1Contract, _setcairo1Contract] = useState<Contract>(new Contract(test1Abi, contractAddress, walletAccountFromContext));
+    const [cairo1Contract, setCairo1Contract] = useState<Contract | undefined>();
 
     async function increaseBalance() {
-        console.log("increase-Cairo1ReadContract=", cairo1Contract.functions);
-        const myCall = cairo1Contract.populate("increase_counter", [10]);
-        console.log("Call=", myCall);
-        walletAccountFromContext?.execute(myCall)
-            .then(async (resp: InvokeFunctionResponse) => {
-                console.log("increaseBalance txH =", resp.transaction_hash);
-                setTransactionHash(resp.transaction_hash);
-                setTransactionResult(await walletAccountFromContext.waitForTransaction(resp.transaction_hash));
-            })
-            .catch((e: any) => { console.log("error increase balance =", e) });
+        if (!!cairo1Contract) {
+            console.log("increase-Cairo1ReadContract=", cairo1Contract.functions);
+            const myCall = cairo1Contract.populate("increase_counter", [10]);
+            console.log("Call=", myCall);
+            walletAccountFromContext?.execute(myCall)
+                .then(async (resp: InvokeFunctionResponse) => {
+                    console.log("increaseBalance txH =", resp.transaction_hash);
+                    setTransactionHash(resp.transaction_hash);
+                    setTransactionResult(await walletAccountFromContext.waitForTransaction(resp.transaction_hash));
+                })
+                .catch((e: any) => { console.log("error increase balance =", e) });
+        }
     }
 
     useEffect(() => {
-        cairo1Contract.get_balance()
-            .then((resp: bigint) => {
-                console.log("resp =", resp)
-                setBalance(Number(resp));
-            })
-            .catch((e: any) => { console.log("error get_balance =", e) });
+        console.log("Contract initialized");
+        setCairo1Contract(new Contract(test1Abi, contractAddress, walletAccountFromContext))
+
+
+        return () => { }
+    }
+        , [walletAccountFromContext]
+    )
+
+    useEffect(() => {
+        console.log("ZZZ");
+        console.log("ZZZ-walletAccountFromContext =", walletAccountFromContext);
+        if (!!cairo1Contract) {
+            console.log("ZZZ-provider", cairo1Contract.providerOrAccount);
+            if (!!walletAccountFromContext) {
+                cairo1Contract.get_balance()
+                    .then((resp: bigint) => {
+                        console.log("resp =", resp)
+                        setBalance(Number(resp));
+                    })
+                    .catch((e: any) => { console.log("error get_balance =", e) });
+            }
+        }
         return () => { }
     }
         , [blockFromContext.block_number, cairo1Contract]); // balance updated at each block
