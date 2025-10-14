16ba0ef570636a6d21503e392605e280804cffcf
PhilippeR26 <philippe.rostan@yahoo.fr>
2025-10-14 11:44:25 +0200

    test: getStarknet v5.0.0 with exported  wallet standard types

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -13,7 +13,7 @@ export default async function Page() {
     return (
             <div>
                 <p className={styles.bgText}>
-                    Test get-starknet v5.0.0ß0 with starknet.js v8.0.0ß2
+                    Test get-starknet v5.0.0 with starknet.js v8.6.0
                 </p>
                 <Center>
                     <Image src={starknetjsImg} alt='starknet.js' width={150} />
